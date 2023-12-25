f27fa363a403d898cc3f7bdb1dbcd3a5a3ea0df4
dkc120 <dilipcyp120A@gmail.com>
2023-12-25 07:56:22 +0530

    added new url

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://fiverrapi.vercel.app/api/",
+  baseURL: "https://fiverrapi-4ja6.onrender.com/api",
   withCredentials: true,
 });
 
